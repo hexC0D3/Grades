cbe9dc2005660612d625fa030d2a0cbc4b22f9d6
Tyratox <tyratox@gmail.com>
2014-08-05 07:44:56 +0200

    Re-added config-sample

---
+++ core/config-sample.php
@@ -0,0 +1,21 @@
+<?php
+
+/** Your database name */
+define('DB_NAME', '###DB_NAME###');
+
+/** Your database user */
+define('DB_USER', '###DB_USER###');
+
+/** Your database password */
+define('DB_PASSWORD', '###DB_PASSWORD###');
+
+/** Your database host, if you don't know what to do try localhost */
+define('DB_HOST', '###DB_HOST###');
+
+/** Your admin username */
+define('ADMIN_USER', '###ADMIN_USER###');
+
+/** Your admin password [Hash => use /admin/setup.php] */
+define('ADMIN_HASH', '###ADMIN_HASH###');
+
+?>
