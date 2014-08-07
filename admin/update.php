8015c58933a9978f52223fdcc1c9d8a5ff89dc9a
Nico Hauser <me@tyratox.cht>
2014-08-07 09:50:13 +0200

    fixes

--- admin/update.php
+++ admin/update.php
@@ -19,7 +19,7 @@ function updateGrades(){
 			foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
 				if(strpos($filename, "/.")===false&&strpos($filename, "/..")===false){
 					$name=explode('Grades-master/', $filename)[1];
-// 					if(strpos(realpath(ROOT_DIR.$name), ROOT_DIR)!==false){ FIXME: dir traversal
+					if(strpos(realpath(ROOT_DIR.$name), ROOT_DIR)!==false){//FIXME can't add new files
 						if(file_exists(ROOT_DIR.$name)){
 							unlink(ROOT_DIR.$name);
 						}
@@ -27,7 +27,7 @@ function updateGrades(){
 						if(file_exists($filename)){
 							unlink($filename);
 						}
-// 					}
+					}
 				}
 			}
 			unlink(ADMIN_DIR.'update.zip');
